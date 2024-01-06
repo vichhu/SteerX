5f1e0fc3e9aa36e476519db6e998f2b3f4708c45
Virochan <125596868+vichhu@users.noreply.github.com>
2024-01-06 12:44:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+//Search Bar
+let search=document.querySelector('.search-box');
+
+document.querySelector('#search-icon').onclick=() => {
+    search.classList.toggle('active');
+}
+
+//Header
+let header=document.querySelector('header');
+
+window.addEventListener('scroll', () =>{
+    header.classList.toggle('shadow',window.scrollY > 0);
+});
